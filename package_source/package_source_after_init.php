275e630b8cd351ebd2470dd79420edf9bc418f0c
Ulrich Schmidt <support@ulicms.de>
2018-03-19 19:57:03 +0100

    https://

--- package_source/package_source_after_init.php
+++ package_source/package_source_after_init.php
@@ -1,3 +1,3 @@
 <?php
 define ( "PACKAGE_SOURCE_BASE_PATH", ULICMS_ROOT . "/packages" );
-define ( "PACKAGE_SOURCE_BAS_URL", "http://packages.ulicms.de" );
+define ( "PACKAGE_SOURCE_BAS_URL", "https://packages.ulicms.de" );
